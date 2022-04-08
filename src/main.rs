6b88a5741e2662ab6dc53cd0534bc5bba7e0ee64
helciogalvao <helciod.galvao@gmail.com>
2022-04-08 20:42:42 -0300

    adicionado funcionalidade ler entrada do usuario

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("Adivinhe o número!");
+    println!("Digite seu palpite.");
+    
+    let mut palpite = String::new();
+    io::stdin().read_line(&mut palpite)
+    .expect("Falha ao ler entrada");
+    
+    println!("Você disse: {}", palpite);
 }
